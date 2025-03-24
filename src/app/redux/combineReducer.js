49410fcc5c7723b41b152731e72f3e78ebcf094f
MENG Vuthvitou <liltou081@gmail.com>
2025-03-24 23:16:17 +0700

    Add redux

---
+++ src/app/redux/combineReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  // add reducers here
+});
+
+export default rootReducer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./_template/assets/css/index.css";
 import AppRoute from "./app/routes/AppRoute.jsx";
+import { Provider } from "react-redux";
+import store from "./app/redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AppRoute />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <AppRoute />
+    </StrictMode>
+  </Provider>
 );
