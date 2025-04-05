4be605e81e5948cda1788eb5663e844cae1bbe6f
MENG Vuthvitou <liltou081@gmail.com>
2025-04-05 09:16:23 +0700

    Remove not used

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "../module/Dashboard/Dashboard";
 import Income from "../module/Income/Income";
 import Expense from "../module/Expense/Expense";
