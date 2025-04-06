8825b209b2a4b265bfb1da66865c0036f3cc60fa
MENG Vuthvitou <liltou081@gmail.com>
2025-04-06 11:13:52 +0700

    Add Loader on RootLayout

--- src/app/layout/RootLayout.jsx
+++ src/app/layout/RootLayout.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import logo from "../../_template/assets/img/small_logo.png";
 import useAuth from "../module/Auth/core/action";
+import Loader from "./components/Loader";
 import {
   RiExchangeDollarLine,
   RiHome2Line,
@@ -16,13 +17,22 @@ const sidebarItems = [
 ];
 
 const RootLayout = () => {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 500);
+    return () => clearTimeout(timer);
+  }, [location]);
   const { onLogout } = useAuth();
 
   return (
     <div className="font-popins">
       <Sidebar logo={logo} onLogout={onLogout} sidebarItems={sidebarItems} />
 
-      <main className="transition duration-500 md:ml-[260px]">
+      <main className="transition relative duration-500 md:ml-[260px]">
+        {loading && <Loader />}
         <Outlet />
       </main>
     </div>
