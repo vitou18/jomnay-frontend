4ae479ade661b9752f3d295e23625647be3f3480
MENG Vuthvitou <liltou081@gmail.com>
2025-04-11 01:29:14 +0700

    Modify some style color

--- src/app/layout/components/HeaderTable.jsx
+++ src/app/layout/components/HeaderTable.jsx
@@ -5,7 +5,7 @@ import { RiAddLine } from "react-icons/ri";
 const HeaderTable = ({onClick}) => {
   return (
     <header className="flex items-center justify-between">
-      <h3 className="text-[18px] md:text-[20px] font-medium">Recent List</h3>
+      <h3 className="text-[18px] md:text-[20px] font-medium text-[#3A3A3A]">Recent List</h3>
 
       <div className="flex items-center gap-x-[10px]">
         <Button
